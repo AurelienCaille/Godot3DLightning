84f83cd2f948649d052fe82cdd3ae21698ab3646
Aurelien <aur-lien@hotmail.fr>
2021-11-04 15:48:25 +0100

    curved mesh mode

--- Lightning3D.gd
+++ Lightning3D.gd
@@ -5,8 +5,10 @@ extends Path
 export(String, "Plane", "Curved") var lightning_mode = "Plane"
 export var bake : bool = false setget set_bake
 export var lightning_material : Material = preload("res://Lightning3D.tres")
+export var lightning_curved_material : Material = preload("res://Lightning3DCurved.tres")
 export var meshs : Array
 export var width : float = 0.5 setget set_width
+export var n_points : int = 4.0
 
 func _ready():
 	connect("curve_changed", self, "_on_path_curve_changed")
@@ -32,20 +34,114 @@ func creates_meshs():
 	
 	if not is_visible_in_tree():
 		return
+		
+		
+	match lightning_mode:
+		"Plane":
+			create_planes_meshs()
+		
+		"Curved":
+			create_curved_meshs()
+			
 	
+	
+func create_curved_meshs():
+	"""
+	"""
+	
+	#prend n points sur la courbe à équidistance
+	#récupère les points à d distance du point
+	#trace le meshs avec l'ensemble de points
+	var curve_length : float = curve.get_baked_length()
+	var points = []
+	
+	for i in range(n_points):
+		var distance_along_curve = curve_length / float(n_points) * float(i)
+		var point : Vector3 = curve.interpolate_baked(distance_along_curve)
+		var point_up : Vector3 = curve.interpolate_baked_up_vector(distance_along_curve)
+		var point_down : Vector3
+		
+		point_up = point_up.normalized() * width / 2.0
+		
+		points.append([point, point_up])
+
+
+	var array_mesh = ArrayMesh.new()
+	var arr = []
+	arr.resize(Mesh.ARRAY_MAX)
+
+	# PoolVectorXXArrays for mesh construction.
+	var verts = PoolVector3Array()
+	var uvs = PoolVector2Array()
+	var normals = PoolVector3Array()
+	var indices = PoolIntArray()
+
+	#######################################
+	## Insert code here to generate mesh ##
+	#######################################
+	for i in range(points.size()-1):
+		var point_info = points[i]
+		var point_next_info = points[i+1]
+		
+		var point : Vector3 = point_info[0]
+		var point_up : Vector3 = point_info[1]
+		
+		var point_next : Vector3 = point_next_info[0]
+		var point_next_up : Vector3 = point_next_info[1]
+		
+		var p1 = point + point_up
+		var p2 = point - point_up
+		var p3 = point_next + point_next_up
+		var p4 = point_next - point_next_up
+		
+		verts.append_array(PoolVector3Array([p1, p2, p3]))
+		verts.append_array(PoolVector3Array([p3, p2, p4]))
+		
+	var colors := []
+	for i in range(verts.size()):
+		colors.append(Color.red)
+		uvs.append(Vector2(1.0/verts.size()*float(i), 1.0/verts.size()*float(i)))
+	arr[Mesh.ARRAY_COLOR] = PoolColorArray(colors)
+
+	# Assign arrays to mesh array.
+	arr[Mesh.ARRAY_VERTEX] = verts
+	arr[Mesh.ARRAY_TEX_UV] = uvs
+	#arr[Mesh.ARRAY_NORMAL] = normals
+	#arr[Mesh.ARRAY_INDEX] = indices
+
+	# Create mesh surface from mesh array.
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arr) # No blendshapes or compression used.
+	ResourceSaver.save("res://sphere.tres", array_mesh, 32)
+	
+	# Create a visual instance (for 3D).
+	var instance = VisualServer.instance_create()
+	# Set the scenario from the world, this ensures it
+	# appears with the same objects as the scene.
+	var world = get_world()
+	var scenario = world.scenario
+	VisualServer.instance_set_scenario(instance, scenario)
+	# Add a mesh to it.
+	# Remember, keep the reference.
+
+	var mesh = array_mesh
+	meshs.append(mesh)
+	
+	VisualServer.instance_set_base(instance, mesh)
+	VisualServer.mesh_surface_set_material(mesh, 0, lightning_curved_material.get_rid())
+	
+	
+
+
+
+func create_planes_meshs():
 	for index_point in curve.get_point_count():
 		if index_point +1 == curve.get_point_count():
 			return
 		
 		var origin : Vector3 = curve.get_point_position(index_point)
 		var extremity : Vector3 = curve.get_point_position(index_point+1)
-	
-		match lightning_mode:
-			"Plane":
-				create_plane_mesh(origin, extremity)
-			
-			"Curved":
-				pass
+		
+		create_plane_mesh(origin, extremity)
 		
 func clean_meshs():
 	for mesh in meshs:
